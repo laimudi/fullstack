5ec2deabe1343c333c1a86a1fecf14b5a87cbd44
laimudi <laimudi46@gmail.com>
2023-03-09 22:36:21 +0700

    Baru

---
+++ database/migrations/2023_03_09_150836_add_produc_id_colum_to_transactions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->unsignedBigInteger('product_id')->required()->after('payment_amount')->nullable();
+            $table->foreign('product_id')->references('id')->on('producs')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropForeign(['product_id']);
+            $table->dropColumn('product_id');
+        });
+    }
+};
